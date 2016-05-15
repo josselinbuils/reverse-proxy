3ae30b3a9171b20884dfc457c06bdfb45ef4893a
Josselin BUILS <josselin.buils@gmail.com>
2016-05-15 14:32:35 +0200

    Use https

--- proxy.js
+++ proxy.js
@@ -11,9 +11,6 @@ const morgan = require('morgan');
 const config = require('./config.json');
 const Logger = require('./logger');
 
-const app = express();
-const proxy = httpProxy.createProxyServer({});
-
 //app.use(morgan('dev'));
 //app.use(helmet());
 
@@ -21,20 +18,24 @@ var lex = LEX.create({
     configDir: require('os').homedir() + '/letsencrypt/etc',
     approveRegistration: function (hostname, cb) {
         cb(null, {
-            domains: ['pizzaday.party', 'www.pizzaday.party'],
+            domains: [hostname],
             email: 'josselin.buils@gmail.com',
             agreeTos: true
         });
     }
 });
 
-http.createServer(LEX.createAcmeResponder(lex, function redirectHttps(req, res) {
+let httpServer = http.createServer(LEX.createAcmeResponder(lex, (req, res) => {
     res.setHeader('Location', 'https://' + req.headers.host + req.url);
     res.statusCode = 302;
     res.end();
-})).listen(80);
+}));
 
+httpServer.listen(80);
 Logger.info('ReverseProxy is listening on port 80 for http protocol');
+
+const app = express();
+const proxy = httpProxy.createProxyServer({});
 
 app.all('*', (req, res) => {
 
@@ -75,6 +76,7 @@ app.all('*', (req, res) => {
     }
 });
 
-https.createServer(lex.httpsOptions, LEX.createAcmeResponder(lex, app)).listen(443);
+let httpsServer = https.createServer(lex.httpsOptions, LEX.createAcmeResponder(lex, app));
 
+httpsServer.listen(443);
 Logger.info('ReverseProxy is listening on port 443 for https protocol');
