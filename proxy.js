3336de20c48c2755f236ad10d756d583841c492c
Josselin BUILS <josselin.buils@gmail.com>
2016-05-15 12:23:37 +0200

    Use link environment variables

--- proxy.js
+++ proxy.js
@@ -41,7 +41,7 @@ app.get('*', (req, res) => {
     }
 
     if (matchingRoute) {
-        proxy.web(req, res, {target: matchingRoute.target});
+        proxy.web(req, res, {target: process.env[matchingRoute.target.toUpperCase() + '_PORT']});
     } else {
         res.status(404).send('Not found');
     }
