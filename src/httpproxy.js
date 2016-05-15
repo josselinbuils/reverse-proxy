2da9a26d42017613b406068b2cd7dd3553eefbac
Josselin BUILS <josselin.buils@gmail.com>
2016-05-15 23:34:38 +0200

    Improved redirection

--- src/httpproxy.js
+++ src/httpproxy.js
@@ -18,14 +18,18 @@ module.exports = class HTTPProxy {
 
         app.use((req, res, next) => {
 
-            if (!/^www\./.test(req.hostname) && req.hostname.split('.').length === 2) {
-                Logger.info(`Add www subdomain to ${req.hostname}`);
-                return res.redirect('http://www.' + req.hostname + req.url);
-            }
+            let addPrefix = !/^www\./.test(req.hostname) && req.hostname.split('.').length === 2,
+                hostname = addPrefix ? 'www' + req.hostname : req.hostname;
 
             if (HTTPSProxy.isHTTPSDomain(req.hostname)) {
-                Logger.info(`${req.hostname} is a HTTPS domain, use HTTPS instead of HTTP`);
-                return res.redirect('https://' + req.hostname + req.url);
+                Logger.info(req.hostname + ' is a HTTPS domain, use HTTPS instead of HTTP');
+
+                Logger.info('Redirect to https://' + hostname + req.url);
+                return res.redirect('https://' + hostname + req.url);
+
+            } else if (addPrefix) {
+                Logger.info('Redirect to http://' + hostname + req.url);
+                return res.redirect('http://' + hostname + req.url);
             }
 
             next();
