ef3736d45f7062a774101c276346e275742ce2be
Josselin BUILS <josselin.buils@gmail.com>
2016-05-16 09:47:40 +0200

    Test

--- proxy.js
+++ proxy.js
@@ -24,7 +24,9 @@ let lex = LEX.create({
         } else {
             Logger.info(hostname + ' is not a HTTPS domain');
         }
-    }
+    },
+    lifetime: 10000,
+    memorizeFor: 10000
 });
 
 Router.init();
