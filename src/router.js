0df4cb01eadbe02d9f5272e2b2187844decaef76
Josselin BUILS <josselin.buils@gmail.com>
2016-05-18 19:19:46 +0200

    Check url

--- src/router.js
+++ src/router.js
@@ -56,7 +56,14 @@ module.exports = class Router {
 
     static redirect(req, res) {
 
-        let url = decodeURIComponent(req.params.url);
+        let url = decodeURIComponent(req.params.url)
+
+        var urlRegex = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+
+        if (!urlRegex.test(url)) {
+            Logger.error('Invalid url: ' + url);
+            return res.status(400).end('Invalid url');
+        }
 
         Logger.info('GET ' + url);
 
