d05544854c33bb23f3808c5b96a9e1e76a86d6ee
Josselin BUILS <josselin.buils@gmail.com>
2018-11-03 15:29:53 +0100

    Adds acme version to greenlock config and cleans code

--- src/proxy.js
+++ src/proxy.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
-const http = require('http');
+const http = require('ht tp');
 const https = require('spdy');
 const leChallengeFs = require('le-challenge-fs');
 const leStoreCertbot = require('le-store-certbot');
@@ -9,43 +9,39 @@ const LEX = require('greenlock-express');
 const Logger = require('./logger');
 const Router = require('./router');
 
-Logger.info('Start ReverseProxy');
+Logger.info('Starts ReverseProxy');
 
 const lex = LEX.create({
-    server: 'https://acme-v01.api.letsencrypt.org/directory',
-    challenges: {
-        'http-01': leChallengeFs.create({}),
-        'tls-sni-01': leChallengeFs.create({})
-    },
-    store: leStoreCertbot.create({
-        configDir: '/letsencrypt/etc',
-        privkeyPath: ':configDir/live/:hostname/privkey.pem',
-        fullchainPath: ':configDir/live/:hostname/fullchain.pem',
-        certPath: ':configDir/live/:hostname/cert.pem',
-        chainPath: ':configDir/live/:hostname/chain.pem',
-        workDir: '/letsencrypt/var/lib',
-        logsDir: '/letsencrypt/var/log',
-        webrootPath: '/letsencrypt/srv/www/:hostname/.well-known/acme-challenge'
-    }),
-    approveDomains: (opts, certs, cb) => {
+  server: 'https://acme-v01.api.letsencrypt.org/directory',
+  version: 'v01',
+  challenges: {
+    'http-01': leChallengeFs.create({}),
+    'tls-sni-01': leChallengeFs.create({}),
+  },
+  store: leStoreCertbot.create({
+    configDir: '/letsencrypt/etc',
+    privkeyPath: ':configDir/live/:hostname/privkey.pem',
+    fullchainPath: ':configDir/live/:hostname/fullchain.pem',
+    certPath: ':configDir/live/:hostname/cert.pem',
+    chainPath: ':configDir/live/:hostname/chain.pem',
+    workDir: '/letsencrypt/var/lib',
+    logsDir: '/letsencrypt/var/log',
+    webrootPath: '/letsencrypt/srv/www/:hostname/.well-known/acme-challenge',
+  }),
+  approveDomains: (options, certs, cb) => {
+    if (certs) {
+      options.domains = certs.altnames;
+    } else {
+      const hostname = options.domains[0];
 
-        if (certs) {
-            opts.domains = certs.altnames;
-        } else {
-            const hostname = opts.domains[0];
+      options.domains = [hostname];
+      options.email = 'josselin.buils@gmail.com';
+      options.agreeTos = Router.isHTTPS(hostname);
 
-            opts.domains = [hostname];
-            opts.email = 'josselin.buils@gmail.com';
-            opts.agreeTos = Router.isHTTPS(hostname);
-
-            Logger.info(`Approve registration for domain ${hostname}: ${opts.agreeTos}`);
-        }
-
-        cb(null, {
-            options: opts,
-            certs: certs
-        });
+      Logger.info(`Approve registration for domain ${hostname}: ${options.agreeTos}`);
     }
+    cb(null, { options, certs });
+  },
 });
 
 const app = express();
@@ -57,10 +53,14 @@ app.use(Router.checkHost);
 app.use(Router.redirectHTTPS);
 app.use(Router.route);
 
-http.createServer(lex.middleware(app)).listen(80, function () {
+http
+  .createServer(lex.middleware(app))
+  .listen(80, function () {
     Logger.info(`ReverseProxy is listening on port ${this.address().port} for HTTP protocol`);
-});
+  });
 
-https.createServer(lex.httpsOptions, lex.middleware(app)).listen(443, function () {
+https
+  .createServer(lex.httpsOptions, lex.middleware(app))
+  .listen(443, function () {
     Logger.info(`ReverseProxy is listening on port ${this.address().port} for HTTPS protocol`);
-});
+  });
--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 const httpProxy = require('http-proxy');
 
+// noinspection JSFileReferences
 const config = require('../config.json');
 const Logger = require('./logger');
 
@@ -7,68 +8,62 @@ const FORBIDDEN = 403;
 const NOT_FOUND = 404;
 
 module.exports = class Router {
+  static checkHost(req, res, next) {
+    const hostConfig = Router.getHostConfig(req.hostname);
 
-    static checkHost(req, res, next) {
-        const hostConfig = Router.getHostConfig(req.hostname);
-
-        if (!hostConfig) {
-            return res.status(FORBIDDEN).end('Unknown host');
-        }
-
-        next();
+    if (!hostConfig) {
+      return res.status(FORBIDDEN).end('Unknown host');
     }
+    next();
+  }
 
-    static getHostConfig(hostname) {
-        return config.hosts[/^www\./.test(hostname) ? hostname.slice(4) : hostname];
+  static getHostConfig(hostname) {
+    return config.hosts[/^www\./.test(hostname) ? hostname.slice(4) : hostname];
+  }
+
+  static init() {
+    Logger.info(`Initializes router`);
+    Router.proxy = httpProxy.createProxyServer({});
+    Router.proxy.on('error', error => Logger.error(`Proxy error: ${error.message}`));
+  }
+
+  static isHTTPS(hostname) {
+    const hostConfig = Router.getHostConfig(hostname);
+    return hostConfig && hostConfig.https;
+  }
+
+  static redirectHTTPS(req, res, next) {
+    const hostConfig = Router.getHostConfig(req.hostname);
+
+    if (req.protocol !== 'https' && hostConfig.https && hostConfig.forceHttps) {
+      const newUrl = `https://${req.hostname}${req.url}`;
+      Logger.info(req.hostname + ' is a HTTPS only domain, use HTTPS instead of HTTP');
+      Logger.info(`Redirect from ${req.protocol}://${req.hostname + req.url} to ${newUrl}`);
+      return res.redirect(newUrl);
     }
+    next();
+  }
 
-    static init() {
-        Logger.info(`Initializes router`);
-        Router.proxy = httpProxy.createProxyServer({});
-        Router.proxy.on('error', error => Logger.error('Proxy error: ' + error.message));
+  static route(req, res) {
+    const hostConfig = Router.getHostConfig(req.hostname);
+    const request = `${req.protocol}://${req.hostname}${req.path}`;
+
+    if (Array.isArray(hostConfig.redirects)) {
+      const redirect = hostConfig.redirects.find(
+        redirect => req.path.toLowerCase().indexOf(redirect.path.toLowerCase()) === 0,
+      );
+
+      if (redirect !== undefined) {
+        const target = `http://${redirect.service}:${redirect.port}`;
+        Logger.info(`${req.method} ${request} -> ${target + req.path}`);
+        Router.proxy.web(req, res, { target: target });
+      } else {
+        Logger.info(`No route found: ${req.method} ${request}`);
+        res.status(NOT_FOUND).end();
+      }
+    } else {
+      Logger.error('Invalid configuration: host property "redirects" shall be an array');
+      res.status(NOT_FOUND).end();
     }
-
-    static isHTTPS(hostname) {
-        const hostConfig = Router.getHostConfig(hostname);
-        return hostConfig && hostConfig.https;
-    }
-
-    static redirectHTTPS(req, res, next) {
-        const hostConfig = Router.getHostConfig(req.hostname);
-
-        if (req.protocol !== 'https' && hostConfig.https && hostConfig.forceHttps) {
-            Logger.info(req.hostname + ' is a HTTPS only domain, use HTTPS instead of HTTP');
-
-            const newUrl = 'https://' + req.hostname + req.url;
-
-            Logger.info(`Redirect from ${req.protocol}://${req.hostname + req.url} to ${newUrl}`);
-            return res.redirect(newUrl);
-        }
-        next();
-    }
-
-    static route(req, res) {
-        const hostConfig = Router.getHostConfig(req.hostname);
-        const request = req.protocol + '://' + req.hostname + req.path;
-
-        let redirect;
-
-        if (Array.isArray(hostConfig.redirects)) {
-            redirect = hostConfig.redirects.find(
-                redirect => req.path.toLowerCase().indexOf(redirect.path.toLowerCase()) === 0
-            );
-
-            if (redirect) {
-                const target = `http://${redirect.service}:${redirect.port}`;
-                Logger.info(`${req.method} ${request} -> ${target + req.path}`);
-                Router.proxy.web(req, res, {target: target});
-            } else {
-                Logger.info(`No route found: ${req.method} ${request}`);
-                res.status(NOT_FOUND).end();
-            }
-        } else {
-            Logger.error('Invalid configuration: host property "redirects" should be an array');
-            res.status(NOT_FOUND).end();
-        }
-    }
+  }
 };
