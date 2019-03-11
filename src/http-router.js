ac48555372afad03b224402779e9b0412a5cebaf
Josselin BUILS <josselin.buils@gmail.com>
2019-03-11 22:01:25 +0100

    Adds error handling in http router

--- src/http-router.js
+++ src/http-router.js
@@ -1,6 +1,6 @@
 const httpProxy = require('http-proxy');
 
-const { ENV_DEV, ENV_PROD, FORBIDDEN, NOT_FOUND } = require('./constants');
+const { ENV_DEV, ENV_PROD, FORBIDDEN, INTERNAL_ERROR, NOT_FOUND } = require('./constants');
 const { Logger } = require('./logger');
 const { getRedirects, getTarget } = require('./routing-helpers');
 
@@ -12,30 +12,35 @@ module.exports.httpRouter = hosts => {
   proxy.on('error', error => Logger.error(`Proxy error: ${error.message}`));
 
   return (req, res) => {
-    const { hostname, method, protocol, url } = req;
-    const redirects = getRedirects(hosts, hostname);
+    try {
+      const { hostname, method, protocol, url } = req;
+      const redirects = getRedirects(hosts, hostname);
 
-    if (!redirects) {
-      return res.status(FORBIDDEN).end('Unknown host');
-    }
+      if (!redirects) {
+        return res.status(FORBIDDEN).end('Unknown host');
+      }
 
-    // Allows HTTP only in dev environment
-    if (protocol !== 'https' && ENV === ENV_PROD) {
-      const newUrl = `https://${hostname}${url}`;
-      Logger.info(hostname + ' is a HTTPS only domain, use HTTPS instead of HTTP');
-      Logger.info(`Redirect from ${protocol}://${hostname}${url} to ${newUrl}`);
-      return res.redirect(newUrl);
-    }
+      // Allows HTTP only in dev environment
+      if (protocol !== 'https' && ENV === ENV_PROD) {
+        const newUrl = `https://${hostname}${url}`;
+        Logger.info(hostname + ' is a HTTPS only domain, use HTTPS instead of HTTP');
+        Logger.info(`Redirect from ${protocol}://${hostname}${url} to ${newUrl}`);
+        return res.redirect(newUrl);
+      }
 
-    const target = getTarget(redirects, protocol, url);
-    const request = `${protocol}://${hostname}${url}`;
+      const target = getTarget(redirects, protocol, url);
+      const request = `${protocol}://${hostname}${url}`;
 
-    if (target) {
-      Logger.info(`${method} ${request} -> ${target}${url}`);
-      proxy.web(req, res, { target });
-    } else {
-      Logger.info(`No HTTP route found: ${method} ${request}`);
-      res.status(NOT_FOUND).end();
+      if (target) {
+        Logger.info(`${method} ${request} -> ${target}${url}`);
+        proxy.web(req, res, { target });
+      } else {
+        Logger.info(`No HTTP route found: ${method} ${request}`);
+        res.status(NOT_FOUND).end();
+      }
+    } catch (error) {
+      Logger.error(error.stack);
+      res.status(INTERNAL_ERROR);
     }
   };
 };
