8171010e3953b6bac8c48acaf26d17e9e56fd5de
Josselin BUILS <josselin.buils@gmail.com>
2019-06-04 20:10:04 +0200

    Fixes bm logs issue
    It was not working anymore because server name was false

--- src/ws-router.js
+++ src/ws-router.js
@@ -1,41 +1,37 @@
-const WebSocket = require('ws');
+const WebSocket = require("ws");
 
-const { Logger } = require('./logger');
-const { getRedirects, getTarget } = require('./routing-helpers');
+const { Logger } = require("./logger");
+const { getRedirects, getTarget } = require("./routing-helpers");
 
 module.exports.wsRouter = hosts => (wsClient, req) => {
-  const { connection, url } = req;
+  const { headers, url } = req;
+  const { host } = headers;
 
-  // Keep same names than in http router to ensure coherence
-  const hostname = connection.servername;
-  const path = url;
-
-  const redirects = getRedirects(hosts, hostname);
+  const redirects = getRedirects(hosts, host);
 
   if (!redirects) {
-    wsClient.send('Unknown host');
+    wsClient.send("Unknown host");
     return wsClient.close();
   }
 
-  const target = getTarget(redirects, 'wss', path);
-  const request = `wss://${hostname}${path}`;
+  const target = getTarget(redirects, "wss", url);
+  const request = `wss://${host}${url}`;
 
   if (target) {
     Logger.info(`${request} -> ${target}`);
 
     const wsProxy = new WebSocket(target);
 
-    wsProxy.on('open', () => {
-      wsClient.on('message', data => wsProxy.send(data));
-      wsProxy.on('message', data => wsClient.send(data));
+    wsProxy.on("open", () => {
+      wsClient.on("message", data => wsProxy.send(data));
+      wsProxy.on("message", data => wsClient.send(data));
     });
 
-    wsClient.on('close', () => wsProxy.close());
-    wsProxy.on('close', () => wsClient.close());
-
+    wsClient.on("close", () => wsProxy.close());
+    wsProxy.on("close", () => wsClient.close());
   } else {
     Logger.info(`No WebSocket route found: ${request}`);
-    wsClient.send('Not found');
+    wsClient.send("Not found");
     return wsClient.close();
   }
 };
